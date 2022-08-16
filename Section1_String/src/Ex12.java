fccbf84c7302c4a8ebbabb1fc5b639265a5a940f
Hogrii <hosboy93@naver.com>
2022-08-16 17:55:27 +0900

    암호 풀이 추가

--- Section1_String/src/Ex12.java
+++ Section1_String/src/Ex12.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Ex12 {
 	public static String solution(int N, String str) throws Exception{
 		String answer = "";
-		int start = 0;
-		int end = 7;	
+		
+//		int start = 0;
+//		int end = 7;	
 		
 		for(int i=0; i<N; i++) {
 			// str을 7자리씩 나누어 pw에 담는다.
-			String pw = str.substring(start, end);
+//			String pw = str.substring(start, end);
+			String pw = str.substring(0, 7);
 			
 			// pw를 0과 1로 정리한다.
 			pw = pw.replace("#", "1");
@@ -21,8 +23,9 @@ public class Ex12 {
 			answer += String.valueOf((char)decimal);					
 			
 			// 다음 pw를 위해 7씩 더해준다.
-			start += 7;
-			end += 7;
+//			start += 7;
+//			end += 7;
+			str = str.substring(7);
 		}		
 		return answer;
 	}
