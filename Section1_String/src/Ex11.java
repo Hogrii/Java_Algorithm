8d4b491c894647e130aa32373924773545224b35
Hogrii <hosboy93@naver.com>
2022-08-16 16:44:07 +0900

    문자열 압축 시작

---
+++ Section1_String/src/Ex11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex11 {
+	public static void main(String[] args) {
+		// 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복될 경우
+		// 문자열을 압축한다.
+		// 반복횟수가 1인 경우 생략
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
