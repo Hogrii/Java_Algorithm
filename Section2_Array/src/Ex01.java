ada799f759b8f003d2b670470fe596f5df565e0f
Hogrii <hosboy93@naver.com>
2022-08-17 17:16:15 +0900

    큰 수 출력하기 완료

--- Section2_Array/src/Ex01.java
+++ Section2_Array/src/Ex01.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ex01 {
+	public static ArrayList<Integer> solution(int[] num) {
+		ArrayList<Integer> answer = new ArrayList<>();
+		
+		answer.add(num[0]);
+		
+		for(int i=1; i<num.length; i++) {
+			if(num[i]>num[i-1]) answer.add(num[i]);
+		}
+		
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		// N개의 정수를 입력받아 자신의 바로 앞 수보다 큰 수만 출력
 		// 첫번째 수는 무조건 출력
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] num = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			num[i] = sc.nextInt();
+		}
+		
+		for(int answer : solution(num)) {
+			System.out.print(answer + " ");
+		}
 	}
 }
