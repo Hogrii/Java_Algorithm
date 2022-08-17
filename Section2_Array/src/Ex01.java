3624a463741efb94a1a60199b1828de9a0e27d19
Hogrii <hosboy93@naver.com>
2022-08-17 17:17:27 +0900

    큰 수 출력하기 풀이 주석 추가

--- Section2_Array/src/Ex01.java
+++ Section2_Array/src/Ex01.java
@@ -5,12 +5,15 @@ public class Ex01 {
 	public static ArrayList<Integer> solution(int[] num) {
 		ArrayList<Integer> answer = new ArrayList<>();
 		
+		// 첫번째 수는 무조건 출력한다.
 		answer.add(num[0]);
 		
 		for(int i=1; i<num.length; i++) {
+			// 현재의 수가 이전의 수보다 크면 추가한다.
 			if(num[i]>num[i-1]) answer.add(num[i]);
 		}
 		
+		// 출력
 		return answer;
 	}
 	
@@ -19,9 +22,11 @@ public class Ex01 {
 		// 첫번째 수는 무조건 출력
 		Scanner sc = new Scanner(System.in);
 		
+		// 받을 정수의 수
 		int N = sc.nextInt();
 		int[] num = new int[N];
 		
+		// 입력받은 정수
 		for(int i=0; i<N; i++) {
 			num[i] = sc.nextInt();
 		}
