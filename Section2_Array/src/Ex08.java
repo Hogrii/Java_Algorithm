f63805eb26ecb4d864ebc60a57917502a406b953
Hogrii <hosboy93@naver.com>
2022-08-23 16:26:12 +0900

    등수 구하기 완료

--- Section2_Array/src/Ex08.java
+++ Section2_Array/src/Ex08.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 
-public class Ex08 {
+public class Ex08 {	
+	public static int[] solution(int N, int[] score){
+		int[] answer = new int[N];
+		
+		for(int i=0; i<score.length; i++) {
+			int ranking = 1;
+			for(int j=0; j<score.length; j++) {
+				if(score[i]<score[j]) ranking++;
+			}
+			answer[i] = ranking;
+		}
+
+
+		return answer;
+	}
 	public static void main(String[] args) {
 		// 등수 구하기
 		// 점수에 따라 등수 출력
 		// 등수가 같다면 높은 등수로 동일 처리
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] score = new int[N];
+		
+		for(int i=0; i<score.length; i++) {
+			score[i] = sc.nextInt();
+		}
+		
+		for(int answer : solution(N, score)) {
+			System.out.print(answer + " ");
+		}
 		
 	}
 }
