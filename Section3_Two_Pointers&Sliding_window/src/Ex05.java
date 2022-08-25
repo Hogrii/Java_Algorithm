8e18b2d79b9fc2d259feb0933695253e9b8be860
Hogrii <hosboy93@naver.com>
2022-08-25 17:27:11 +0900

    연속된 자연수의 합 완료

--- Section3_Two_Pointers&Sliding_window/src/Ex05.java
+++ Section3_Two_Pointers&Sliding_window/src/Ex05.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 
 public class Ex05 {
+	public static int solution(int N) {
+		int answer = 0;
+		int sum = 0;
+		int cnt = 0;
+		int min = 1;
+		
+		for(int i=1; i<N; i++) {
+			sum += i;
+			if(sum==N) cnt++;
+			while(sum>N) {
+				sum -= min;
+				min++;
+				if(sum==N) cnt++;
+			}
+		}
+		
+		answer = cnt;
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		// 연속된 자연수의 합
 		// 양의 정수 N 입력
+		Scanner sc = new Scanner(System.in);
 		
+		int N = sc.nextInt();
+		
+		System.out.println(solution(N));
 	}
 }
