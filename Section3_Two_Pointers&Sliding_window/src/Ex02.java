00e6b8dd6c40d648d1d62691e7b06d39d8b0f1b2
Hogrii <hosboy93@naver.com>
2022-08-25 15:50:47 +0900

    공통원소 구하기 풀이 주석 추가

--- Section3_Two_Pointers&Sliding_window/src/Ex02.java
+++ Section3_Two_Pointers&Sliding_window/src/Ex02.java
@@ -7,18 +7,24 @@ public class Ex02 {
 		ArrayList<Integer> answer = new ArrayList<>();
 		int indexA = 0, indexB = 0;
 		
+		// 각 배열을 오름차순으로 정렬
 		Arrays.sort(A);
 		Arrays.sort(B);
 		
+		// 각 배열의 인덱스가 각 배열의 길이보다 작을 때
 		while(indexA<N && indexB<M) {
+			// 두 값이 서로 같으면 answer에 추가 후 각 인덱스 증가
 			if(A[indexA]==B[indexB]) {
 				answer.add(A[indexA++]);
 				indexB++;
 			}
+			// B배열의 값이 더 크면 A배열의 인덱스 증가
 			else if(A[indexA]<B[indexB]) indexA++;
+			// A배열의 값이 더 크면 B배열의 인덱스 증가
 			else indexB++;
 		}
 		
+		// 출력
 		return answer;
 	}
 	
