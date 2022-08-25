1af3f3d46952be11d69c8e880b0988639c1e98d0
Hogrii <hosboy93@naver.com>
2022-08-25 15:49:30 +0900

    공통원소 구하기 완료

--- Section3_Two_Pointers&Sliding_window/src/Ex02.java
+++ Section3_Two_Pointers&Sliding_window/src/Ex02.java
@@ -1,7 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Ex02 {
+	public static ArrayList<Integer> solution(int N, int M, int[] A, int[] B){
+		ArrayList<Integer> answer = new ArrayList<>();
+		int indexA = 0, indexB = 0;
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		while(indexA<N && indexB<M) {
+			if(A[indexA]==B[indexB]) {
+				answer.add(A[indexA++]);
+				indexB++;
+			}
+			else if(A[indexA]<B[indexB]) indexA++;
+			else indexB++;
+		}
+		
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		// 공통 원소 구하기
 		// A, B 두 집합의 공통 원소를 오름차순으로 출력
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		for(int i=0; i<N; i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		int M = sc.nextInt();
+		int[] B = new int[M];
+		for(int i=0; i<M; i++) {
+			B[i] = sc.nextInt();
+		}
+		
+		for(int answer : solution(N, M, A, B)) System.out.print(answer + " ");
 	}
 }
