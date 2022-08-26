f14cf033e356b02144530ae7d76ca5bfe510e686
Hogrii <hosboy93@naver.com>
2022-08-26 15:40:38 +0900

    학급 회장(해쉬) - 풀이 완료 - 주석 추가 - HashMap 관련 다양한 함수 공부

--- Section4_HashMap_TreeSet/src/Ex01.java
+++ Section4_HashMap_TreeSet/src/Ex01.java
@@ -2,12 +2,47 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Ex01 {
-	public static char solution(int n, String str) {
+	public static char solution(int n, String votes) {
 		char answer = ' ';
+		HashMap<Character, Integer> voteMap = new HashMap<>();
+		
+		/*
 		// Key는 Char, Value는 int
 		HashMap<Character, Integer> map = new HashMap<>();
 		map.put('A', 3);
+		 */
 		
+		for(char vote : votes.toCharArray()) {
+			// .getOrDefault()는 값이 있으면 vote값을 가져오고 없으면 0값을 가져온다.
+			// 카운팅에 아주 중요!
+			voteMap.put(vote, voteMap.getOrDefault(vote, 0)+1);
+		}
+		
+		// Integer.MIN_VALUE는 정수 중 가장 작은 값을 가져온다.
+		// 최대값을 갱신하기 위해 가장 작은 값을 가져오는 것
+		int max = Integer.MIN_VALUE;
+		
+		// .ketSet()은 HashMap 타입으로 선언된 객체의 키들을 탐색한다.
+		for(char key : voteMap.keySet()) {
+			// .get(key)은 HashMap에 저장되어 있는 key에 해당하는 value를 가져온다.
+			// System.out.println(key + " " + voteMap.get(key));
+			if(voteMap.get(key)>max) {
+				max = voteMap.get(key);
+				answer = key;
+			}
+		}
+		
+		// .containsKey()는 해당하는 key에 대한 value가 있는지 확인하는 것
+		// 키가 존재하면 true, 존재하지 않으면 false를 출력
+		// System.out.println(voteMap.containsKey('A'));
+		
+		// .size()는 현재 HashMap에 등록되어 있는 key의 수를 가져온다.
+		// System.out.println(voteMap.size());
+		
+		// .remove()는 key를 삭제한다.
+		// System.out.println(voteMap.remove());
+		
+		// 출력
 		return answer;
 	}
 	
@@ -19,9 +54,8 @@ public class Ex01 {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		String str = sc.next();
+		String votes = sc.next();
 		
-		System.out.println(solution(n, str));
-		
+		System.out.println(solution(n, votes));		
 	}
 }
