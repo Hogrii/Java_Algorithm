a5423ef60a107235caecaa24df1ea9c30beadbf0
Hogrii <hosboy93@naver.com>
2022-08-26 15:21:38 +0900

    HashMap 정의

--- Section4_HashMap_TreeSet/src/Ex01.java
+++ Section4_HashMap_TreeSet/src/Ex01.java
@@ -1,9 +1,27 @@
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Ex01 {
+	public static char solution(int n, String str) {
+		char answer = ' ';
+		// Key는 Char, Value는 int
+		HashMap<Character, Integer> map = new HashMap<>();
+		map.put('A', 3);
+		
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		// 학급 회장(해쉬)
 		// 학생 수 N 입력
 		// N개의 투표용지 입력(A~E)
 		// 회장으로 선출된 학생 출력
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String str = sc.next();
+		
+		System.out.println(solution(n, str));
+		
 	}
 }
