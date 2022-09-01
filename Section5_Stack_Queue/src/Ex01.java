ca0cfeee3d4cabb3c16c2961e7145e41c0dc72fd
Hogrii <hosboy93@naver.com>
2022-09-01 22:47:44 +0900

    올바른 괄호 스택 설명 및 풀이 완료

--- Section5_Stack_Queue/src/Ex01.java
+++ Section5_Stack_Queue/src/Ex01.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Ex01 {
+	public static String solution(String str) {
+		String answer = "YES";
+		// Stack 객체 생성
+		Stack<Character> stack = new Stack<>();
+		
+		for(char bracket : str.toCharArray()) {
+			if(bracket == '(') stack.push(bracket);
+			else {
+				if(stack.isEmpty()) return "NO";
+				stack.pop();
+			}
+		}
+		if(!stack.isEmpty()) answer = "NO";
+		
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		// 올바른 괄호
 		// 괄호가 입력되었을 때 올바른 괄호면 "YES", 아니면 "NO" 출력
 		// 올바른 괄호는 쌍이 올바르게 위치함을 의미
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		System.out.println(solution(str));
 	}
 }
