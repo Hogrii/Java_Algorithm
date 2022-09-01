08f755ef4f2e8cf1c89c2d09641f9d04f200fac3
Hogrii <hosboy93@naver.com>
2022-09-01 22:48:57 +0900

    올바른 괄호 풀이 주석 추가

--- Section5_Stack_Queue/src/Ex01.java
+++ Section5_Stack_Queue/src/Ex01.java
@@ -8,14 +8,20 @@ public class Ex01 {
 		Stack<Character> stack = new Stack<>();
 		
 		for(char bracket : str.toCharArray()) {
+			// 여는 괄호가 오면 stack에 쌓는다.
 			if(bracket == '(') stack.push(bracket);
+			// 닫는 괄호가 왔을 때
 			else {
+				// stack이 비어 있으면 올바르지 못한 괄호이기 때문에 NO 리턴
 				if(stack.isEmpty()) return "NO";
+				// stack이 비어있지 않으면 맨 위에 있는 stack를 꺼낸다.
 				stack.pop();
 			}
 		}
+		// 만약 모든 과정이 끝났을 때 stack에 뭔가 쌓여있다면 NO 리턴
 		if(!stack.isEmpty()) answer = "NO";
 		
+		// 출력
 		return answer;
 	}
 	
