34089202246c2ee460e8d98b0a9c637972ddf042
Hogrii <hosboy93@naver.com>
2022-11-08 16:47:24 +0900

    괄호문자제거 주석 추가

--- Section5_Stack_Queue/src/Ex02.java
+++ Section5_Stack_Queue/src/Ex02.java
@@ -8,22 +8,28 @@ public class Ex02 {
 		String answer = "";
 		Stack<Character> stack = new Stack<>();
 		
+		// 입력받은 문자열을 하나씩 체크
 		for(char c : str.toCharArray()) {
+			// 입력받은 문자가 )일 경우 (가 나올때까지 pop
 			if(c==')') {
 				while(stack.pop()!='(');
+			// 입력받은 문자가 )가 아니면 모두 push
 			}else stack.push(c);
 		}
 		
+		// stack에 남아있는 문자를 answer에 담는다.
 		for(int i=0; i<stack.size(); i++) answer += stack.get(i);
 
 		return answer;
 	}
 	
 	public static void main(String[] args) throws IOException{
+		// 괄호문자제거
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String str = br.readLine();
 		
+		// 출력		
 		System.out.println(solution(str));		
 	}
 }
