7bf37e668ab7fb8b7257931b51563fbdba0335fb
Hogrii <hosboy93@naver.com>
2022-11-08 18:31:50 +0900

    후위식 연산(postfix) 풀이

--- Section5_Stack_Queue/src/Ex04.java
+++ Section5_Stack_Queue/src/Ex04.java
@@ -1,6 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class Ex04 {
-	public static void main(String[] args) {
+public class Ex04 {	
+	public static int solution(String str) {
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+		
+		for(char letter : str.toCharArray()) {
+			if(Character.isDigit(letter)) {
+				stack.push(letter-48);
+			}
+			else {
+				int rightNum = stack.pop();
+				int leftNum = stack.pop();
+				if(letter == '+') stack.push(leftNum+rightNum);
+				else if(letter == '-') stack.push(leftNum-rightNum);
+				else if(letter == '*') stack.push(leftNum*rightNum);
+				else if(letter == '/') stack.push(leftNum/rightNum);
+			}
+		}
+		answer = stack.pop();
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) throws IOException{
 		// 후위식 연산(postfix)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		System.out.println(solution(str));
 	}
 }
