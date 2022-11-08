307fac840c7b5439981e179741551dd64c79be22
Hogrii <hosboy93@naver.com>
2022-11-08 18:04:02 +0900

    크레용 인형뽑기(카카오) 주식 추가

--- Section5_Stack_Queue/src/Ex03.java
+++ Section5_Stack_Queue/src/Ex03.java
@@ -11,14 +11,21 @@ public class Ex03 {
 		
 		for(int i=0; i<moves.length; i++) {
 			for(int j=0; j<board.length; j++) {
+				// 크레인이 닿는 배열의 값이 0이 아닌 경우 = 인형이 있음
 				if(board[j][moves[i]-1]!=0) {
+					// 해당 자리의 인형을 tmp에 담는다.
 					int tmp = board[j][moves[i]-1];
+					// 해당 자리의 인형을 뽑았기 때문에 0으로 바꿔준다.
 					board[j][moves[i]-1] = 0;
+					// 현재 뽑은 인형의 맨 윗자리가 비어있지 않고, 현재 뽑은 인형과 같은 인형이라면
 					if(!stack.empty() && stack.peek()==tmp) {
+						// 인형을 터뜨린다.
 						answer += 2;
 						stack.pop();
 					}
+					// if에 해당하지 않으면 뽑은 인형을 stack의 맨 위에 올려둔다.
 					else stack.push(tmp);
+					// 크레인이 인형에 한번이라도 닿으면 프로그램을 종료해야하기 떄문에 반드시 break로 for문을 나가준다.
 					break;
 				}
 			}
