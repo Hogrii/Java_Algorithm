422083bd23caced08eb34d9b25cbc15f9b4d135b
Hogrii <hosboy93@naver.com>
2022-11-08 18:35:12 +0900

    후위식 연산(postfix) 주석 추가

--- Section5_Stack_Queue/src/Ex04.java
+++ Section5_Stack_Queue/src/Ex04.java
@@ -9,18 +9,23 @@ public class Ex04 {
 		Stack<Integer> stack = new Stack<>();
 		
 		for(char letter : str.toCharArray()) {
+			// 문자열의 값이 숫자인지 연산자인지 확인
 			if(Character.isDigit(letter)) {
+				// 숫자이면 char에서 48을 빼서 값을 얻는다.
 				stack.push(letter-48);
 			}
 			else {
+				// 문자열의 값이 연산자라면 pop을 이용해 숫자를 꺼낸 뒤 연산을 진행한다.
 				int rightNum = stack.pop();
 				int leftNum = stack.pop();
+				// 연산자 종류
 				if(letter == '+') stack.push(leftNum+rightNum);
 				else if(letter == '-') stack.push(leftNum-rightNum);
 				else if(letter == '*') stack.push(leftNum*rightNum);
 				else if(letter == '/') stack.push(leftNum/rightNum);
 			}
 		}
+		// 마지막 남은 값을 answer에 담아 출력
 		answer = stack.pop();
 		
 		return answer;
