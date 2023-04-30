5a863e7610b2c3d1c3e25fffe6e3fcb77e7300b7
LeeJinHo <hosboy93@naver.com>
2023-04-30 11:30:43 +0900

    DFS 팩토리얼

--- Section7_Recursive_Function/src/Ex03.java
+++ Section7_Recursive_Function/src/Ex03.java
@@ -2,14 +2,7 @@
 public class Ex03 {
 	public int DFS(int n) {
 		if(n==1) return 1;
-		
-		return n * DFS(n-1);
-		// D(5)
-		// -> 5 * D(4)
-		// -> 5 * 4 * D(3)
-		// -> 5 * 4 * 3 * D(2)
-		// -> 5 * 4 * 3 * 2 * D(1)
-		// -> 5 * 4 * 3 * 2 * 1
+		else return n*DFS(n-1);
 	}
 	
 	public static void main(String[] args) {
